8f3db476d0e1047aa0f06d097a5d7e45a6d47220
Chris Müller <chris@krue.ml>
2019-09-23 22:18:31 +0200

    [TASK] Use .editorconfig from TYPO3 core

--- Resources/Public/JavaScript/TableCheck.js
+++ Resources/Public/JavaScript/TableCheck.js
@@ -1,59 +1,59 @@
 define(['jquery'], function($) {
-    'use strict';
+  'use strict';
 
-    var TableCheck = {};
+  var TableCheck = {};
 
-    TableCheck.init = function() {
-        window.addEventListener('load', function() {
-            var listElement = document.getElementById('jobrouter-data-table-list');
+  TableCheck.init = function() {
+    window.addEventListener('load', function() {
+      var listElement = document.getElementById('jobrouter-data-table-list');
 
-            if (!listElement) {
-                return;
-            }
+      if (!listElement) {
+        return;
+      }
 
-            listElement.addEventListener('click', function(event) {
-                var linkElement = event.target.closest('.jobrouter-data-table-check');
+      listElement.addEventListener('click', function(event) {
+        var linkElement = event.target.closest('.jobrouter-data-table-check');
 
-                if (!linkElement) {
-                    return;
-                }
+        if (!linkElement) {
+          return;
+        }
 
-                event.preventDefault();
+        event.preventDefault();
 
-                TableCheck.check(
-                    linkElement.dataset.tableUid,
-                    linkElement.dataset.tableName
-                );
-            });
-        });
+        TableCheck.check(
+          linkElement.dataset.tableUid,
+          linkElement.dataset.tableName
+        );
+      });
+    });
+  };
+
+  TableCheck.check = function(id, name) {
+    var url = top.TYPO3.settings.ajaxUrls['jobrouter_data_table_check'];
+    var settings = {
+      type: 'POST',
+      data: {tableId: +id}
     };
 
-    TableCheck.check = function(id, name) {
-        var url = top.TYPO3.settings.ajaxUrls['jobrouter_data_table_check'];
-        var settings = {
-            type: 'POST',
-            data: {tableId: +id}
-        };
+    var notificationTitle = TYPO3.lang['table_check_for'] + ' ' + name;
+    $.ajax(url, settings)
+      .done(function(data) {
+        if (data.check === 'ok') {
+          top.TYPO3.Notification.success(notificationTitle, TYPO3.lang['table_check_successful'], 5);
+          return;
+        }
 
-        var notificationTitle = TYPO3.lang['table_check_for'] + ' ' + name;
-        $.ajax(url, settings)
-            .done(function(data) {
-                if (data.check === 'ok') {
-                    top.TYPO3.Notification.success(notificationTitle, TYPO3.lang['table_check_successful'], 5);
-                    return;
-                }
+        if (data.error) {
+          top.TYPO3.Notification.error(notificationTitle, data.error);
+          return;
+        }
 
-                if (data.error) {
-                    top.TYPO3.Notification.error(notificationTitle, data.error);
-                    return;
-                }
+        top.TYPO3.Notification.error(notificationTitle, 'Unknown error');
+      })
+      .fail(function(jqXhr, textStatus) {
+        top.TYPO3.Notification.error(notificationTitle, 'Unknown error (' + textStatus + ', ' + jqXhr.status + ')');
+      });
+  };
 
-                top.TYPO3.Notification.error(notificationTitle, 'Unknown error');
-            })
-            .fail(function(jqXhr, textStatus) {
-                top.TYPO3.Notification.error(notificationTitle, 'Unknown error (' + textStatus + ', ' + jqXhr.status + ')');
-            });
-    };
-
-    TableCheck.init();
+  TableCheck.init();
 });
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,30 +1,28 @@
-CREATE TABLE tx_jobrouterdata_domain_model_table
-(
-    type smallint(5) unsigned DEFAULT '0' NOT NULL,
-    name varchar(30)  DEFAULT '' NOT NULL,
-    connection int(11) unsigned DEFAULT '0' NOT NULL,
-    table_guid varchar(36) DEFAULT '' NOT NULL,
-    local_table varchar(100) DEFAULT '' NOT NULL,
-    columns int(11) unsigned DEFAULT '0' NOT NULL,
-    datasets int(11) unsigned DEFAULT '0' NOT NULL,
+CREATE TABLE tx_jobrouterdata_domain_model_table (
+	type smallint(5) unsigned DEFAULT '0' NOT NULL,
+	name varchar(30)  DEFAULT '' NOT NULL,
+	connection int(11) unsigned DEFAULT '0' NOT NULL,
+	table_guid varchar(36) DEFAULT '' NOT NULL,
+	local_table varchar(100) DEFAULT '' NOT NULL,
+	columns int(11) unsigned DEFAULT '0' NOT NULL,
+	datasets int(11) unsigned DEFAULT '0' NOT NULL
 );
 
 CREATE TABLE tx_jobrouterdata_domain_model_column (
-    table_uid int(11) unsigned DEFAULT '0' NOT NULL,
-    name varchar(20) DEFAULT ''  NOT NULL,
-    label varchar(255) DEFAULT ''  NOT NULL,
-    type smallint(5) unsigned DEFAULT '0' NOT NULL,
-    decimal_places smallint(5) unsigned DEFAULT '0' NOT NULL,
+	table_uid int(11) unsigned DEFAULT '0' NOT NULL,
+	name varchar(20) DEFAULT ''  NOT NULL,
+	label varchar(255) DEFAULT ''  NOT NULL,
+	type smallint(5) unsigned DEFAULT '0' NOT NULL,
+	decimal_places smallint(5) unsigned DEFAULT '0' NOT NULL,
 
-    KEY table_uid (table_uid),
+	KEY table_uid (table_uid)
 );
 
-CREATE TABLE tx_jobrouterdata_domain_model_dataset
-(
-    table_uid int(11) unsigned DEFAULT '0' NOT NULL,
-    jrid int(11) unsigned DEFAULT '0' NOT NULL,
-    dataset text,
+CREATE TABLE tx_jobrouterdata_domain_model_dataset (
+	table_uid int(11) unsigned DEFAULT '0' NOT NULL,
+	jrid int(11) unsigned DEFAULT '0' NOT NULL,
+	dataset text,
 
-    UNIQUE KEY tableuid_jrid (table_uid, jrid),
-    KEY table_uid (table_uid),
+	UNIQUE KEY tableuid_jrid (table_uid, jrid),
+	KEY table_uid (table_uid)
 );
