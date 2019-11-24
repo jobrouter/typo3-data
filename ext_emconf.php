fd08b0db6f02d4eeb1ed0f8207a897f2c061cb3b
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2019-11-24 18:11:28 +0100

    [TASK] Set requirement from typo3-jobrouter-connector to version 0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
-            'jobrouter_connector' => '0.3.0-0.0.0',
+            'jobrouter_connector' => '0.4.0-0.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
