f2ffb6ffde98519dab45175e60a5cc633d04fe32
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2019-12-10 19:13:20 +0100

    [TASK] Use variable for extension key

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,17 +1,19 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Brotkrueml.JobRouterData',
-    'Pi',
-    [
-        'Table' => 'show',
-    ]
-);
+(function ($extensionKey = 'jobrouter_data') {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Brotkrueml.JobRouterData',
+        'Pi',
+        [
+            'Table' => 'show',
+        ]
+    );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-    '@import "EXT:jobrouter_data/Configuration/TypoScript/"'
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        '@import "EXT:' . $extensionKey . '/Configuration/TypoScript/"'
+    );
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['jobRouterData'] =
-    ['Brotkrueml\\JobRouterData\\ViewHelpers'];
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['jobRouterData'] =
+        ['Brotkrueml\\JobRouterData\\ViewHelpers'];
+})();
