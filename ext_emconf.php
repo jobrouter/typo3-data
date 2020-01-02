132416acdd3108d0f1a491becba2a47b79ee553a
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-01-02 20:50:00 +0100

    [RELEASE] Prepare version 0.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,11 +6,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Chris Müller',
     'author_email' => 'typo3@krue.ml',
     'state' => 'beta',
-    'version' => '0.4.0-dev',
+    'version' => '0.4.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
-            'jobrouter_connector' => '0.4.0-0.0.0',
+            'jobrouter_connector' => '0.5.0-0.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
