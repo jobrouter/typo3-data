f8556e603185a8223ea7f6be12ef804d9546a515
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-01-11 19:52:49 +0100

    [TASK] Use jobrouter-connector/0.6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,11 +6,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Chris Müller',
     'author_email' => 'typo3@krue.ml',
     'state' => 'beta',
-    'version' => '0.4.0',
+    'version' => '0.5.0-dev',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
-            'jobrouter_connector' => '0.5.0-0.0.0',
+            'jobrouter_connector' => '0.6.0-0.6.99',
         ],
         'conflicts' => [],
         'suggests' => [],
