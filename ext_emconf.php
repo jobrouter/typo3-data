1f0e08ab0b8bf7dac67f71bac875c2f4c50b8476
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-07-23 17:17:26 +0200

    [TASK] Set version to 0.11.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,11 +6,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Chris Müller',
     'author_email' => 'typo3@krue.ml',
     'state' => 'beta',
-    'version' => '0.10.1-dev',
+    'version' => '0.11.0-dev',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-10.4.99',
-            'jobrouter_connector' => '0.10.0-0.10.99',
+            'jobrouter_connector' => '0.11.0-0.11.99',
         ],
         'conflicts' => [],
         'suggests' => [],
