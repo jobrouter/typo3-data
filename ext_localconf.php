96351c76cf3ae548bdf317ce329747c9d2ea6a09
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-12-06 18:47:58 +0100

    [TASK] Remove superfluous Fluid namespace configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,3 @@ defined('TYPO3') || die();
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
     '@import "EXT:' . Brotkrueml\JobRouterData\Extension::KEY . '/Configuration/TypoScript/"'
 );
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['jobRouterData'] =
-    ['Brotkrueml\\JobRouterData\\ViewHelpers'];
