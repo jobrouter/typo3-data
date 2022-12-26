185b7e87214746627c23d61e2f1ab4746c7963e2
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-12-26 09:50:24 +0100

    [TASK] Update typo3/testing-framework to version 7

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -41,7 +41,7 @@ Successfully tested with docker version 19.03.13-ce and docker-compose 1.25.0.
 
 Usage: $0 [options] [file]
 
-No arguments: Run all unit tests with PHP 7.4
+No arguments: Run all unit tests with PHP 8.1
 
 Options:
     -s <...>
@@ -65,9 +65,9 @@ Options:
             - postgres: use postgres
             - sqlite: use sqlite
 
-    -p <7.2|7.3|7.4|8.0>
+    -p <8.1|8.2>
         Specifies the PHP minor version to be used
-            - 7.4 (default): use PHP 7.4
+            - 8.1 (default): use PHP 8.1
 
     -e "<phpunit or codeception options>"
         Only with -s acceptance|functional|unit
@@ -99,11 +99,11 @@ Options:
         Show this help.
 
 Examples:
-    # Run unit tests using PHP 7.4
+    # Run unit tests using PHP 8.1
     ./Build/Scripts/runTests.sh
 
-    # Run unit tests using PHP 7.3
-    ./Build/Scripts/runTests.sh -p 7.3
+    # Run unit tests using PHP 8.2
+    ./Build/Scripts/runTests.sh -p 8.2
 EOF
 
 # Test if docker-compose exists, else exit out with error
@@ -124,12 +124,12 @@ cd ../testing-docker || exit 1
 ROOT_DIR=`readlink -f ${PWD}/../../`
 TEST_SUITE="unit"
 DBMS="mariadb"
-PHP_VERSION="7.4"
+PHP_VERSION="8.1"
 PHP_XDEBUG_ON=0
 PHP_XDEBUG_PORT=9000
 EXTRA_TEST_OPTIONS=""
 SCRIPT_VERBOSE=0
-TYPO3="10"
+TYPO3="11"
 
 # Option parsing
 # Reset in case getopts has been used previously in the shell
@@ -137,7 +137,7 @@ OPTIND=1
 # Array for invalid options
 INVALID_OPTIONS=();
 # Simple option parsing based on getopts (! not getopt)
-while getopts ":s:d:p:e:t:xy:nhuvf" OPT; do
+while getopts ":s:d:p:e:t:xy:huv" OPT; do
     case ${OPT} in
         s)
             TEST_SUITE=${OPTARG}
@@ -190,7 +190,7 @@ if [ ${#INVALID_OPTIONS[@]} -ne 0 ]; then
     exit 1
 fi
 
-# Move "7.2" to "php72", the latter is the docker container name
+# Move "8.1" to "php81", the latter is the docker container name
 DOCKER_PHP_IMAGE=`echo "php${PHP_VERSION}" | sed -e 's/\.//'`
 
 # Set $1 to first mass argument, this is the optional test file or test directory to execute
