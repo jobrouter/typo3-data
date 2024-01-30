0451e4f8eb0a4daa4cbcc9cec9b962e2f3b9bf73
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-01-30 14:03:15 +0100

    chore: update rector to 0.19.5

--- rector.php
+++ rector.php
@@ -3,25 +3,22 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\FunctionLike\AddReturnTypeDeclarationFromYieldsRector;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $config): void {
     $config->phpVersion(PhpVersion::PHP_81);
 
     $config->sets([
         LevelSetList::UP_TO_PHP_81,
+        PHPUnitSetList::PHPUNIT_100,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION,
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
     ]);
 
     $config->autoloadPaths([
