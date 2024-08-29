1e2f9de4a0c7be8ce34f089bb75c9e84b954b924
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-08-29 09:41:44 +0200

    chore: remove module registration in ext_tables.php
    related: #23

--- ext_tables.php
+++ ext_tables.php
@@ -8,26 +8,5 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die();
 
-if ((new Typo3Version())->getMajorVersion() === 11) {
-    ExtensionManagementUtility::addModule(
-        'jobrouter',
-        'data',
-        '',
-        '',
-        [
-            'routeTarget' => TableListController::class . '::handleRequest',
-            'access' => 'admin',
-            'name' => Extension::MODULE_NAME,
-            'iconIdentifier' => 'jobrouter-module-data',
-            'labels' => Extension::LANGUAGE_PATH_BACKEND_MODULE,
-            'workspaces' => 'online',
-        ]
-    );
-
-    ExtensionManagementUtility::addPageTSConfig(
-        '@import "EXT:' . Extension::KEY . '/Configuration/page.tsconfig"'
-    );
-}
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] =
     TableUpdateHook::class;
