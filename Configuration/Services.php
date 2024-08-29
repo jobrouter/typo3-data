bf2ea98d19c8ace696df7347363ea2bf6b3637ef
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-08-29 13:35:54 +0200

    refactor: drop Services.yaml in favour of Services.php

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -13,6 +13,11 @@ namespace JobRouter\AddOn\Typo3Data;
 
 use JobRouter\AddOn\Typo3Base\Widgets\TransferReportWidget;
 use JobRouter\AddOn\Typo3Base\Widgets\TransferStatusWidget;
+use JobRouter\AddOn\Typo3Data\EventListener\DateFormatter;
+use JobRouter\AddOn\Typo3Data\EventListener\DateTimeFormatter;
+use JobRouter\AddOn\Typo3Data\EventListener\DecimalFormatter;
+use JobRouter\AddOn\Typo3Data\EventListener\IntegerFormatter;
+use JobRouter\AddOn\Typo3Data\EventListener\ToolbarItemProvider;
 use JobRouter\AddOn\Typo3Data\Widgets\Provider\TransferReportDataProvider;
 use JobRouter\AddOn\Typo3Data\Widgets\Provider\TransferStatusDataProvider;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -21,48 +26,100 @@ use Symfony\Component\DependencyInjection\Reference;
 use TYPO3\CMS\Dashboard\Dashboard;
 
 return static function (ContainerConfigurator $configurator, ContainerBuilder $containerBuilder): void {
-    if (! $containerBuilder->hasDefinition(Dashboard::class)) {
-        return;
-    }
-
     $services = $configurator->services();
+    $services
+        ->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->private();
 
     $services
-        ->set('dashboard.widget.jobrouter.typo3_data.statusOfDataTransmissions')
-        ->class(TransferStatusWidget::class)
-        ->arg('$dataProvider', new Reference(TransferStatusDataProvider::class))
-        ->arg(
-            '$options',
-            [
-                'refreshAvailable' => true,
-            ],
-        )
-        ->tag('dashboard.widget', [
-            'identifier' => 'jobrouter_data.statusOfDataTransmissions',
-            'groupNames' => 'jobrouter',
-            'title' => Extension::LANGUAGE_PATH_DASHBOARD . ':widgets.statusOfDataTransmissions.title',
-            'description' => Extension::LANGUAGE_PATH_DASHBOARD . ':widgets.statusOfDataTransmissions.description',
-            'iconIdentifier' => 'content-widget-number',
-            'height' => 'small',
-        ]);
+        ->load('JobRouter\AddOn\Typo3Data\\', '../Classes/*')
+        ->exclude('../Classes/{Domain/Dto,Domain/Entity,Exception,Extension.php,Hooks,UserFunctions}');
 
     $services
-        ->set('dashboard.widget.jobrouter.typo3_data.transferReport')
-        ->class(TransferReportWidget::class)
-        ->arg('$dataProvider', new Reference(TransferReportDataProvider::class))
-        ->arg(
-            '$options',
+        ->set(ToolbarItemProvider::class)
+        ->tag(
+            'event.listener',
             [
-                'refreshAvailable' => true,
+                'identifier' => 'jobrouter-data/toolbar-item-provider',
             ],
-        )
-        ->tag('dashboard.widget', [
-            'identifier' => 'jobrouter_data.transferReport',
-            'groupNames' => 'jobrouter',
-            'title' => Extension::LANGUAGE_PATH_DASHBOARD . ':widgets.transferReport.title',
-            'description' => Extension::LANGUAGE_PATH_DASHBOARD . ':widgets.transferReport.description',
-            'iconIdentifier' => 'content-widget-table',
-            'height' => 'medium',
-            'width' => 'large',
-        ]);
+        );
+
+    $services
+        ->set(IntegerFormatter::class)
+        ->tag(
+            'event.listener',
+            [
+                'identifier' => 'jobrouter-data/integer-formatter',
+            ],
+        );
+
+    $services
+        ->set(DecimalFormatter::class)
+        ->tag(
+            'event.listener',
+            [
+                'identifier' => 'jobrouter-data/decimal-formatter',
+            ],
+        );
+
+    $services
+        ->set(DateFormatter::class)
+        ->tag(
+            'event.listener',
+            [
+                'identifier' => 'jobrouter-data/date-formatter',
+            ],
+        );
+
+    $services
+        ->set(DateTimeFormatter::class)
+        ->tag(
+            'event.listener',
+            [
+                'identifier' => 'jobrouter-data/datetime-formatter',
+            ],
+        );
+
+    if ($containerBuilder->hasDefinition(Dashboard::class)) {
+        $services
+            ->set('dashboard.widget.jobrouter.typo3_data.statusOfDataTransmissions')
+            ->class(TransferStatusWidget::class)
+            ->arg('$dataProvider', new Reference(TransferStatusDataProvider::class))
+            ->arg(
+                '$options',
+                [
+                    'refreshAvailable' => true,
+                ],
+            )
+            ->tag('dashboard.widget', [
+                'identifier' => 'jobrouter_data.statusOfDataTransmissions',
+                'groupNames' => 'jobrouter',
+                'title' => Extension::LANGUAGE_PATH_DASHBOARD . ':widgets.statusOfDataTransmissions.title',
+                'description' => Extension::LANGUAGE_PATH_DASHBOARD . ':widgets.statusOfDataTransmissions.description',
+                'iconIdentifier' => 'content-widget-number',
+                'height' => 'small',
+            ]);
+
+        $services
+            ->set('dashboard.widget.jobrouter.typo3_data.transferReport')
+            ->class(TransferReportWidget::class)
+            ->arg('$dataProvider', new Reference(TransferReportDataProvider::class))
+            ->arg(
+                '$options',
+                [
+                    'refreshAvailable' => true,
+                ],
+            )
+            ->tag('dashboard.widget', [
+                'identifier' => 'jobrouter_data.transferReport',
+                'groupNames' => 'jobrouter',
+                'title' => Extension::LANGUAGE_PATH_DASHBOARD . ':widgets.transferReport.title',
+                'description' => Extension::LANGUAGE_PATH_DASHBOARD . ':widgets.transferReport.description',
+                'iconIdentifier' => 'content-widget-table',
+                'height' => 'medium',
+                'width' => 'large',
+            ]);
+    }
 };
